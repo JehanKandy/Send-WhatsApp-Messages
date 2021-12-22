75a491998cc1797192f44f65d63170d7c99ae77f
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-12-22 16:56:28 +0530

    Add files via upload

---
+++ send_mess.py
@@ -0,0 +1,7 @@
+import pywhatkit
+pywhatkit.sendwhatmsg("+xx xxxxxxxxx","Hi All",16,49)
+
+
+# +xx xxxxxxxxx enter any mobile Number
+# "Hi All" you can type any message here
+# 16,48 ---> time that you want to send the message
